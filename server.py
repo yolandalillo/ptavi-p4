e5dae3e8bf3971af87fe34cd93d73c7e6af0de3e
Gregorio Robles <grex@gsyc.urjc.es>
2017-10-18 21:35:45 +0200

    Adding comments to the server

--- server.py
+++ server.py
@@ -13,12 +13,19 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
 
     def handle(self):
+        """
+        handle method of the server class
+        (all requests will be handled by this method)
+        """
         self.wfile.write(b"Hemos recibido tu peticion")
         for line in self.rfile:
             print("El cliente nos manda ", line.decode('utf-8'))
 
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    # Listens at localhost ('') port 6001 
+    # and calls the EchoHandler class to manage the request
+    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
+
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
