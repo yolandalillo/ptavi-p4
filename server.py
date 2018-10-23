6dffe45a9190385866d7eb4ada2fedb3e6449fea
Yolanda Lillo Mata <ylillo@beta15.aulas.gsyc.urjc.es>
2018-10-23 14:54:17 +0200

    Servidor configurado

--- server.py
+++ server.py
@@ -5,7 +5,9 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
+PORT = int(sys.argv[1]) #A la hora de ejecutar poner el puerto en el servidor que no me de fallo
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -18,14 +20,15 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         (all requests will be handled by this method)
         """
         self.wfile.write(b"Hemos recibido tu peticion")
+        IPcliente = self.client_address [0]
+        PORTcliente = self.client_address[1]
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+            print("El cliente nos manda ", line.decode('utf-8'), IPcliente, PORTcliente)
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
-
+    serv = socketserver.UDPServer(('', PORT), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
