3d5ec07d34f7fdfcad8712c0c2991ec2f9fed0a5
Yolanda Lillo Mata <ylillo@alpha27.aulas.gsyc.urjc.es>
2018-10-25 10:38:53 +0200

    Ejercicio7 cliente

--- client.py
+++ client.py
@@ -12,19 +12,22 @@ SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 LINE = sys.argv[3:]
 
-
-
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    entire = ' '.join(LINE)
-    if entire != "":
-        if LINE[0] == "register":
-            entire = "REGISTER sip:" + LINE[1] + " SIP/2.0\r\n\r\n"
-        my_socket.send(bytes(entire, 'utf-8') + b'\r\n')
-        data = my_socket.recv(1024)
-        print(data.decode('utf-8'))
-    else:
-        print("No hay mensaje =>")
 
-print("Socket terminado.")
+try:
+    SERVER, PORT, METHOD, USER, EXVAL = sys.argv[1:]
+except ValueError:
+    sys.exit("Debe introducir los siguientes datos: client.py ip "
+             "puerto register sip_address expires_value")
+REG = ("REGISTER sip:" + USER + " SIP/2.0\r\nExpires: " + EXVAL + "\r\n\r\n")
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+    my_socket.connect((SERVER, int(PORT)))
+    my_socket.send(bytes(REG, 'utf-8') + b'\r\n')
+
+    try:
+        data = my_socket.recv(1024).decode('utf-8')
+    except ConnectionRefusedError:
+        sys.exit("NO se puede conectar al servidor")
+    print(data)
