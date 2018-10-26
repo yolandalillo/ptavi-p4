a3adf63c0f1166fd798e90a0353171531b1cad96
Yolanda Lillo Mata <ylillo@alpha26.aulas.gsyc.urjc.es>
2018-10-26 16:38:37 +0200

    diccionario borrado

--- server.py
+++ server.py
@@ -1,41 +1,41 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Clase (y programa principal) para un servidor de eco en UDP simple
+Clase (y programa principal) para un servidor de SIP en UDP simple
 """
 
 import socketserver
 import sys
-
+import json
+import time
 
 class SIPRegistrerHandler(socketserver.DatagramRequestHandler):
     """
         SIP server class
     """
-    dic = {}
+
     def handle(self):
 
         while 1:
-            # Leyendo lo que envia el cliente
+            #Leyendo lo que envia el cliente.
             line = self.rfile.read()
             line_client = line.decode('utf-8').split()
             if not line:
-                break
+                break #Termina el bucle
             else:
-                print("Petición recibida \r\n" )
+                print("Petición recibida \r\n")
 
             if line_client[0] == 'REGISTER':
-                # Guardamos dirección e IP en nuestro diccionario
+                #Guardamos dirección e IP en nuestro diccionario.
                 direccion = line_client[1].split(':')
                 dic[direccion[1]] = self.client_address[0]
-                print("Enviamos SIP/2.0 200 OK\r\n")
+                print("SIP/2.0 200 OK\r\n")
                 self.wfile.write(b"SIP/2.0 200 OK" + b'\r\n\r\n')
-                print(dic)
-                if line_client[3] == '0':
+                if line_client[4] == '0':
                     print("Borramos del diccionario")
                     del dic[direccion[1]]
-                    self.wfile.write(b"SIP/2.0 200 OK" + b'\r\n\r\n')
-
+                else:
+                    print(dic)
 
 if __name__ == "__main__":
 
@@ -44,13 +44,11 @@ if __name__ == "__main__":
         PORT = int(sys.argv[1])
     except IndexError:
         sys.exit("Debe introducir: server.py port_number")
-    #Creamos UDP en el puerto que indicamos utilizando la clase
+    """Creamos UDP en el puerto que indicamos utilizando la clase."""
     serv = socketserver.UDPServer(('', PORT), SIPRegistrerHandler)
     print("Iniciando servidor... \r\n")
     try:
-        #Creamos el servidor
+        """Creamos el servidor"""
         serv.serve_forever()
     except KeyboardInterrupt:
         print("Finalizado servidor")
-
-
