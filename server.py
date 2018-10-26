ddca6cdfa104e000bb6731234af9206970b7408b
Yolanda Lillo Mata <ylillo@alpha27.aulas.gsyc.urjc.es>
2018-10-26 10:43:55 +0200

    Editado para que si faltan datos el servidor mande mensaje

--- server.py
+++ server.py
@@ -7,11 +7,11 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import socketserver
 import sys
 
-PORT = int(sys.argv[1])
-
 
 class SIPRegistrerHandler(socketserver.DatagramRequestHandler):
-
+    """
+        SIP server class
+    """
     dic = {}
 
     def handle(self):
@@ -22,17 +22,22 @@ class SIPRegistrerHandler(socketserver.DatagramRequestHandler):
         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
         print(self.client_address)
         for line in self.rfile:
-            if line:
-                if line.decode('utf-8')[:8] == 'REGISTER':
-                    print("El cliente nos envia:", line.decode('utf-8'))
-                    user = line.decode('utf-8')[13:-10]
-                    self.dic[user] = self.client_address[0]
+            if line.decode('utf-8')[:8] == 'REGISTER':
+                print("El cliente nos envia:", line.decode('utf-8'))
+                user = line.decode('utf-8')[13:-10]
+                self.dic['DIRECCION'] = user
+                self.dic['IP'] = self.client_address[0]
         print(self.dic)
 
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
+    try:
+        PORT = int(sys.argv[1])
+    except IndexError:
+        sys.exit("Debe introducir: server.py port_number")
+
     serv = socketserver.UDPServer(('', PORT), SIPRegistrerHandler)
     print("Lanzando servidor UDP de SIP...")
     try:
